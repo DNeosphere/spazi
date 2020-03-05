882bf64073ecc6005c3f04ccdd703010cc899805
Dani Gomez <daniela.gomezp05@outlook.com>
2020-03-05 14:07:26 -0500

    FEtch

--- spazi-app/src/components/Email.js
+++ spazi-app/src/components/Email.js
@@ -21,16 +21,23 @@ class Email extends React.Component {
         } else if (emailPattern.test(this.state.email)){
             const url = 'http://localhost:5001/registers'
             let data = {email: this.state.email}
-            console.log(data)
-            fetch(url, {
-                method: 'POST',
-                body: JSON.stringify(data),
-                headers:{
+
+            async function postData(url = '', data = {}) {
+                // Default options are marked with *
+                const response = await fetch(url, {
+                  method: 'POST', // *GET, POST, PUT, DELETE, etc.
+                  mode: 'cors', // no-cors, *cors, same-origin
+                    headers: {
                     'Content-Type': 'application/json'
+                    },
+                  body: JSON.stringify(data) // body data type must match "Content-Type" header
+                });
+                return await response.json(); // parses JSON response into native JavaScript objects
                 }
-            }).then(res => console.log(res.json()))
-                .catch(error => console.error('Error:', error))
-                .then(response => console.log('Success:', response));
+                postData(url, data)
+                .then((data) => {
+                  console.log(data); // JSON data parsed by `response.json()` call
+                });
         } else {
             alert('Invalid email, check again :)')
         }
