0e7474895fae9b062d15c11492c8f35720610830
Dani Gomez <daniela.gomezp05@outlook.com>
2020-03-10 17:05:20 -0500

    [FEature] routesok

--- spazi-app/src/pages/Signup.js
+++ spazi-app/src/pages/Signup.js
@@ -60,7 +60,7 @@ class SignUp extends Component {
               </div>
 
               <div className="FormField">
-                  <button className="FormField__Button mr-20">Sign Up</button> <Link to="/sign-in" className="FormField__Link">I'm already member</Link>
+                  <button className="FormField__Button mr-20">Sign Up</button> <Link to="/signin" className="FormField__Link">I'm already member</Link>
               </div>
             </form>
           </div>
