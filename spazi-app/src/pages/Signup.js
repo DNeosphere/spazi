129bdca6894a33441d2823fd2d2e4ea135f14187
Dioni1195 <dionisio.arango95@gmail.com>
2020-03-10 17:33:18 -0500

    [Dioni] : Adding SigUp view

--- spazi-app/src/pages/Signup.js
+++ spazi-app/src/pages/Signup.js
@@ -29,8 +29,46 @@ class SignUp extends Component {
         });
     }
 
-    handleSubmit(e) {
-        e.preventDefault();
+    handleSubmit(event) {
+        const emailPattern = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        event.preventDefault();
+
+        if (this.state.email === '') {
+            alert('Please enter an Email')
+        } else if (emailPattern.test(this.state.email)){
+            const url = 'https://spazi.rocks/api/users'
+            let data = {
+                email: this.state.email,
+                password: this.state.password,
+                name: this.state.name
+            }
+
+            async function postData(url = '', data = {}) {
+                // Default options are marked with *
+                const response = await fetch(url, {
+                  method: 'POST', // *GET, POST, PUT, DELETE, etc.
+                  //mode: 'no-cors',
+                    headers: {
+                    'Content-Type': 'application/json'
+                    },
+                  body: JSON.stringify(data) // body data type must match "Content-Type" header
+                });
+                if (response.status === 200) {
+                    alert('User created succesfully')
+                }
+                return await response.json().catch(err => console.log(err.meessage)) // parses JSON response into native JavaScript objects
+            }
+            try{
+                const resp = postData(url, data)
+
+            } catch (err){
+                alert('Something went wrong')
+                console.log(err.message)
+            }
+        } else {
+            alert('Invalid email, check again :)')
+        }
+        //this.setState( {email: ''} )
 
         console.log('The form was submitted with the following data:');
         console.log(this.state);
