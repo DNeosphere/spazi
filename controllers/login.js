b946fd1325b34b38389446af750dbc0263561272
Carlos Zuluaga <carlosez9@hotmail.com>
2020-03-10 21:10:19 -0500

    [Charles]: Add login controller

---
+++ controllers/login.js
@@ -0,0 +1,49 @@
+const mongoose = require('mongoose');
+const User  = mongoose.model('User');
+const jwt = require('jsonwebtoken');
+
+const secret = process.env.SPAZI_JWT_SECRET;
+
+exports.loginUser = function(req, res) {
+    const { email, password } = req.body;
+    User.findOne({ email }, function(err, user) {
+      if (err) {
+        console.error(err);
+        res.status(500)
+          .json({
+          status: 'error',
+          message: 'Internal error please try again'
+        });
+      } else if (!user) {
+        res.status(401)
+          .json({
+            status: 'error',
+            message: 'Incorrect email or password'
+          });
+      } else {
+        user.isCorrectPassword(password, function(err, same) {
+          if (err) {
+            res.status(500)
+              .json({
+                status: 'error',
+                message: 'Internal error please try again'
+            });
+          } else if (!same) {
+            res.status(401)
+              .json({
+                status: 'error',
+                message: 'Incorrect email or password'
+            });
+          } else {
+            // Issue token
+            const payload = { id: user._id, email };
+            const token = jwt.sign(payload, secret, {
+              expiresIn: '3h'
+            });
+            res.cookie('token', token, { httpOnly: true })
+              .sendStatus(200);
+          }
+        });
+      }
+    });
+  };
