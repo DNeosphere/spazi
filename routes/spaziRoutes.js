4436df0a8a56aa99a92fed001b920c6e4baf3324
Carlos Zuluaga <carlosez9@hotmail.com>
2020-03-12 16:21:14 -0500

    [Charles]: Protect routes with token (not specific for spazis)

--- routes/spaziRoutes.js
+++ routes/spaziRoutes.js
@@ -1,12 +1,13 @@
 const express = require('express');
+const withAuth = require('../util/auth.js');
 
 const router = new express.Router();
 const Spazi = require('../controllers/spazi');
 
 router.route('/spazis').get(Spazi.findAllSpazis);
-router.route('/spazis/:id').get(Spazi.findSpaziByID);
-router.route('/spazis/:id').put(Spazi.updateSpazi);
+router.route('/spazis/:id').get(withAuth, Spazi.findSpaziByID);
+router.route('/spazis/:id').put(withAuth, Spazi.updateSpazi);
 router.route('/spazis').post(Spazi.createSpazi);
-router.route('/spazis/:id').delete(Spazi.deleteSpazi);
+router.route('/spazis/:id').delete(withAuth, Spazi.deleteSpazi);
 
 module.exports = router;
