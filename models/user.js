136b162a235a046a43cf0e7b40e23f43b177af05
Carlos Zuluaga <carlosez9@hotmail.com>
2020-03-16 20:27:33 -0500

    [Charles]: Modified models

--- models/user.js
+++ models/user.js
@@ -21,6 +21,7 @@ const userSchema = new Schema({
   name:    { type: String, required: true },
   email: { type: String, required: true, unique: true},
   password: { type: String, required: true},
+  imageUrl : { type: String },
   contact: contactSchema,
   subject: [subjectSchema],
   reviews:  { type: String }
