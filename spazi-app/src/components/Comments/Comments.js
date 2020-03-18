6af1aef26a77d14d753839c87b68a32b4e3aef8e
Carlos Zuluaga <carlosez9@hotmail.com>
2020-03-18 13:39:32 -0500

    [Charles]: Modified comments section

--- spazi-app/src/components/Comments/Comments.js
+++ spazi-app/src/components/Comments/Comments.js
@@ -1,30 +1,21 @@
 import React from 'react'
+import './Comments.css';
 
 function Comments () {
     return (
-        <div className="row">
-            <div className="primary col">
+        <div className="row" id="what-is">
+            <div className="primary col centered-column-unique">
                 <h2>What do we do</h2>
                 <p>Spazi is the App you were looking for!</p>
                 <p>With Spazi, you can go on vacations and your cat, your dog or even your plants are going to be ok!
+                </p>
+                <p>
                     Spazi provides the connection between people who need to leave their loved ones in safe hands and
                     the Spazis, persons who are passionate about animals
                 </p>
                 <p className='special-p'>
                    "You just need to love them, <br/> we will take care of them for you"
                 </p>
-
-            </div>
-            
-            <div className="secondary col">
-                <h2>Find a Spazi or be a Spazi</h2>
-                <p><strong>If you love animals </strong> and you have time and to take care of them, then
-                you can <strong>be a Spazi!</strong> we will be happy to hear from you.<br/>
-                We also love animals, that is why we have an application process to ensure the safety of them.
-                </p>
-                <p><strong>Find a Spazi! </strong> We understand the life rythm of these times, when you don't
-                have the time or you have to travel and can't take care of your loved ones, we will be there With
-                the perfect Spazi for you and your pet.</p>
 
             </div>
 	</div>
