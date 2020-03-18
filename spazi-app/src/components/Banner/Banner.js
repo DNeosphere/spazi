8e7f363a1d9fb4fd7624a32610ec15d256d93933
Carlos Zuluaga <carlosez9@hotmail.com>
2020-03-18 13:12:50 -0500

    [Charles]: Add Navbar and button

--- spazi-app/src/components/Banner/Banner.js
+++ spazi-app/src/components/Banner/Banner.js
@@ -1,5 +1,5 @@
 import React from  'react'
-
+import { Link } from "react-router-dom";
 import './Banner.css';
 
 function Banner () {
@@ -9,6 +9,7 @@ function Banner () {
             <div className="banner-text">
                 <h1 className="headline">The Care App</h1>
                 <p className="tagline">Relax, we will take care of them for you</p>
+                <Link to='signup'><input className='get-started-btn' type="submit" value="Get started" /></Link>
             </div>
            
         </div>
