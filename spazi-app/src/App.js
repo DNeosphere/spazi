2eb7d507a0afa15e7ad5e4e72543d70f228b472a
Dioni1195 <dionisio.arango95@gmail.com>
2020-03-19 21:44:12 -0500

    [Dioni] : Handle spazis and protecting spazis routes

--- spazi-app/src/App.js
+++ spazi-app/src/App.js
@@ -1,11 +1,12 @@
 import React from "react";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 import "./styles/base.css";
 import "./styles/flexbox.css";
 
-import { Landing, Home, SignIn, SignUp, SignUpSpazi, Users, Logout } from "./pages";
-import {AuthComponent} from "./components";
+import { Landing, Home, SignIn, SignUp, SignUpSpazi, Users, Logout, SignInSpazi, Spazis } from "./pages";
+import { AuthComponent, AuthComponentSpazi } from "./components";
+
 
 
 function App() {
@@ -15,15 +16,21 @@ function App() {
         <Route path="/" exact component={Home} />
         <Route path="/landing" component={Landing} />
         <Route path="/signin" component={SignIn} />
+        <Route path="/signinspazi" component={SignInSpazi} />
         <Route path="/signup" component={SignUp} />
         <Route path="/signupspazi" component={SignUpSpazi} />
         <Route path="/logout" component={Logout} />
+        
         <AuthComponent>
           <Route path="/users" component={Users} />
           <Route path="/find-spazis" />
           <Route path="/history" />
           <Route path="/profile" />
         </AuthComponent>
+        
+        <AuthComponentSpazi>
+          <Route path="/spazis" component={Spazis} />
+        </AuthComponentSpazi>
 
       </Switch>
     </Router>
